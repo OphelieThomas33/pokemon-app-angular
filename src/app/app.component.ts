3915dfa5b5e3ab29dd523c3ab74efbad44101822
OphelieThomas33 <ophelie_thomas@hotmail.com>
2023-10-20 13:00:41 +0200

    add components list-pokemon + detail-pokemon + routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list';
-import { Pokemon } from './pokemon';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
   
 })
-export class AppComponent implements OnInit {
-  pokemonList: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon|undefined;
-
-  // déconseillé de rédiger un constructeur dans un projet Angular
-  // constructor() {
-  //   this.pokemonList = [];  
-  // }
-
-  ngOnInit(): void {
-      console.table(this.pokemonList);
-  }
-
-  // définir une méthode :
-  selectPokemon(pokemonId: string) {
-    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
-    if(pokemon) {
-      console.log(`Vous avez cliqué sur le pokemon ${pokemon.name}`);
-      this.pokemonSelected = pokemon;
-    } else {
-      console.log(`Vous avez demandé un pokemon qui n'existe pas`);
-      this.pokemonSelected = pokemon;
-    }
-  
-  }
+export class AppComponent {
 
 }
