7238501540064e43be5a64cbc3223cb53598e264
OphelieThomas33 <ophelie_thomas@hotmail.com>
2023-10-23 15:26:39 +0200

    creation auth

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -11,13 +11,15 @@ import { PokemonFormComponent } from './pokemon-form/pokemon-form.component';
 import { EditPokemonComponent } from './edit-pokemon/edit-pokemon.component';
 import { AddPokemonComponent } from './add-pokemon/add-pokemon.component';
 import { SearchPokemonComponent } from './search-pokemon/search-pokemon.component';
+import { LoaderComponent } from './loader/loader.component';
+import { AuthGuard } from '../auth.guard';
 
 
 const pokemonRoutes: Routes = [
-  { path: 'edit/pokemon/:id', component: EditPokemonComponent},
-  { path: 'pokemon/add', component: AddPokemonComponent},
-  { path: 'pokemons', component: ListPokemonComponent},
-  { path: 'pokemon/:id', component: DetailPokemonComponent}, // : permet de dire que c'est une variable à récupérer
+  { path: 'edit/pokemon/:id', component: EditPokemonComponent, canActivate: [AuthGuard]},
+  { path: 'pokemon/add', component: AddPokemonComponent, canActivate: [AuthGuard]},
+  { path: 'pokemons', component: ListPokemonComponent, canActivate: [AuthGuard]},
+  { path: 'pokemon/:id', component: DetailPokemonComponent, canActivate: [AuthGuard]}, // : permet de dire que c'est une variable à récupérer
 ];
 
 @NgModule({
@@ -30,6 +32,7 @@ const pokemonRoutes: Routes = [
     EditPokemonComponent,
     AddPokemonComponent,
     SearchPokemonComponent,
+    LoaderComponent,
   ],
   imports: [
     CommonModule,
